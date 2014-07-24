6355ee2f70fd1d7b83003c0a85b51b5dbdcc3325
Tapas Behera <tapasbehera@tapass-mbp.home>
2014-07-23 20:46:26 -0500

    Android Wear sample for Voice command

---
+++ wear/src/main/java/tapas/example/com/voicecommand/RunActivity.java
@@ -0,0 +1,57 @@
+    package tapas.example.com.voicecommand;
+
+    import android.app.Activity;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.speech.RecognizerIntent;
+    import android.support.wearable.view.WatchViewStub;
+    import android.view.MotionEvent;
+    import android.view.View;
+    import android.widget.TextView;
+
+    import java.util.List;
+
+    public class RunActivity extends Activity implements View.OnTouchListener {
+
+        private static final int SPEECH_REQUEST_CODE = 0;
+
+        private TextView mTextView;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_run);
+            mTextView = (TextView) findViewById(R.id.textView);
+            mTextView.setOnTouchListener(this);
+        }
+
+
+        // Create an intent that can start the Speech Recognizer activity
+        private void displaySpeechRecognizer() {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+    // Start the activity, the intent will be populated with the speech text
+                startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        }
+
+     // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode,
+                                        Intent data) {
+            if (resultCode == RESULT_OK) {
+                List<String> results = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                String spokenText = results.get(0);
+                mTextView.setText(spokenText);
+                }
+           super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            displaySpeechRecognizer();
+            return true;
+        }
+    }
